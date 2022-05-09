cbc81b6e4a9b12a743fa1d31b6872e9d2a281c67
Ari <Arianna2028@users.noreply.github.com>
2022-05-09 09:53:48 -0400

    docs: add comments to strategy.py for clarification, info, and example

--- modules/custom-access-flow/strategy.py
+++ modules/custom-access-flow/strategy.py
@@ -1,17 +1,56 @@
 from sym.sdk.strategies import AccessStrategy
 from sym.sdk.integrations import slack
+import requests
 
 
 class CustomAccess(AccessStrategy):
+    """This AccessStrategy is fully customizable, and contains basic implementations
+    of all three required methods.
+    """
+
     def fetch_remote_identity(self, user):
+        # If the user doesn't have an identity already for this strategy, use
+        # and save their email address as their identity. This will be returned by
+        # self.get_requester_identity.
         return user.email
 
     def escalate(self, target_id, event):
+        # Retrieve the user's identity for this strategy
         requester = self.get_requester_identity(event)
+
+        # Retrieve the identifier field from the requested target's settings
         target_identifier = event.payload.fields["target"].settings["identifier"]
+
+        # Message the requesting user on Slack that their access has been granted
         slack.send_message(slack.user(requester), f"Access to {target_identifier} granted!")
 
+        # Example:
+        # Here is where you might hit an external API to grant access to the requester
+        # response = requests.post(
+        #     "https://some-saas-product.com/api/v1/grant-access",
+        #     json={"email": requester}
+        # )
+
+        # if not response.ok:
+        #     raise RuntimeError(f"Failed to grant access: {response.json()['error']}")
+
+
     def deescalate(self, target_id, event):
+        # Retrieve the user's identity for this strategy
         requester = self.get_requester_identity(event)
+
+        # Retrieve the identifier field from the requested target's settings
         target_identifier = event.payload.fields["target"].settings["identifier"]
+
+        # Message the requesting user on Slack that their access has been granted
         slack.send_message(slack.user(requester), f"Access to {target_identifier} revoked!")
+
+        # Example:
+        # Here is where you might hit an external API to revoke access from the requester
+        # response = requests.post(
+        #     "https://some-saas-product.com/api/v1/revoke-access",
+        #     json={"email": requester}
+        # )
+
+        # if not response.ok:
+        #     raise RuntimeError(f"Failed to revoke access: {response.json()['error']}")
